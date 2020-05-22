bf4599ed7ab77dd0c651b6be0f03e6d9adbb5e86
VitaliiRadaiev <hf.chelenger@gmail.com>
2020-05-22 12:09:24 +0300

    fix img, js, html

--- app/js/main.js
+++ app/js/main.js
@@ -172,6 +172,16 @@ $(function(){
 			document.webkitCancelFullScreen();
 		}
 	}
+
+	let mainVideoPlayer = document.querySelector('.resoftables_video_player');
+
+	mainVideoPlayer.onclick = () => {
+
+		let videoWrap = document.querySelector('.resoftables_video');
+		videoWrap.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/65oQLS7ZzHE?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	}
+
+	//<iframe width="560" height="315" src="https://www.youtube.com/embed/65oQLS7ZzHE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 });
 
 
